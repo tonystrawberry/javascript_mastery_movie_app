1c789abcddec54279fafdfa805fb7d0b1845e838
Tony Duong <tony.duong.102@gmail.com>
2026-02-01 13:37:41 +0900

    Update app.json and refactor index screen with new components
    - Renamed app name and updated icon paths in app.json.
    - Modified adaptive icon background color and favicon.
    - Refactored index screen to use functional component with hooks for fetching movies.
    - Added SearchBar component for improved movie search functionality.
    - Implemented loading and error handling for movie data retrieval.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -9,7 +9,7 @@ function TabIcon({ focused, icon, title }: any) {
     return (
       <ImageBackground
         source={images.highlight}
-        className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+        className="flex flex-row w-full flex-1 min-w-[100px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
       >
         <Image source={icon} tintColor="#151312" className="size-5" />
         <Text className="text-secondary text-base font-semibold ml-2">
