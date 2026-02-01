736ea0239c0d200a0925ea1b3631d0df704872b5
Tony Duong <tony.duong.102@gmail.com>
2026-02-01 14:31:40 +0900

    Enhance search functionality and add movie card component
    - Updated Tailwind configuration to include components directory for styling.
    - Refactored search screen to implement a functional component with search capabilities, including debounced input and loading/error states.
    - Introduced MovieCard component for displaying movie details in a grid format.
    - Added Appwrite service for managing search counts and trending movies.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,14 @@
 import { Stack } from "expo-router";
+import { useEffect } from "react";
 import "./globals.css";
+import { client } from "../services/appwrite";
 
 export default function RootLayout() {
+  useEffect(() => {
+    // Ping Appwrite backend server to verify the setup
+    client.ping();
+  }, []);
+
   return <Stack>
     <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     <Stack.Screen name="movie/[id]" options={{ headerShown: false }} />
--- services/appwrite.ts
+++ services/appwrite.ts
@@ -3,9 +3,9 @@ import { Client, Databases, ID, Query } from "react-native-appwrite";
 const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
 const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
 
-const client = new Client()
-  .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!);
+export const client = new Client()
+  .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!)
+  .setEndpoint("https://sgp.cloud.appwrite.io/v1");
 
 const database = new Databases(client);
 
